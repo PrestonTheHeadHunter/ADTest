df5f01bac2e44028210bb630c8f585e208011f98
Preston Hale <preston.hale@avanade.com>
2018-02-19 14:09:39 -0600

    Add project files.

---
+++ ADTest/Controllers/CalcController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ADTest.Models;
+using System.IO;
+
+namespace ADTest.Controllers
+{
+    public class CalcController : ApiController
+    {
+
+        [Route("api/add")]
+        [HttpPost]
+        public HttpResponseMessage Add([FromUri]CalcInput input)
+        {
+
+            HttpResponseMessage response = Request.CreateResponse();
+
+            return response;
+        }
+
+        [Route("api/subtract")]
+        [HttpPost]
+        public HttpResponseMessage Subtract([FromUri]CalcInput input)
+        {
+
+            HttpResponseMessage response = Request.CreateResponse();
+
+            return response;
+        }
+    }
+}
---
+++ ADTest/Models/CalcInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ADTest.Models
+{
+    public class CalcInput
+    {
+        [JsonProperty(PropertyName = "a")]
+        public int a { get; set; }
+
+
+        [JsonProperty(PropertyName = "b")]
+        public int b { get; set; }
+    }
+}
